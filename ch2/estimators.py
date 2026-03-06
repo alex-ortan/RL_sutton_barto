8abcbcc27068e7be61903d3c69ca0f1bcb603473
alex-ortan <13038064+alex-ortan@users.noreply.github.com.>
2026-03-06 10:37:46 -0500

    bugfix: np.full was generating an array of integers without the dtype=float parameter

--- ch2/estimators.py
+++ ch2/estimators.py
@@ -1,36 +1,32 @@
-import numpy as np
-
 from typing import Protocol
-from typing import Callable
+
+import numpy as np
 
 
 class Estimator(Protocol):
     """This defines the 'Contract'."""
+
     k: int
     n: np.ndarray
     q: np.ndarray
+
     def update(self, action: int, reward: float) -> None: ...
 
 
-class SampleAverageEstimator(object):
-    """ 
+class SampleAverageEstimator:
+    """
     Keeps and updates estimates Q(a) for all arms a of a k-armed bandit.
     """
 
-    def __init__(self, k_arms=10, initial_q=0):
+    def __init__(self, k_arms=10, *, initial_q=0):
         self.k = k_arms
-        self.n = np.zeros(k_arms)           # number of times each action was taken
-        self.q = np.full(k_arms, initial_q, dtype=float) # values estimates for each action
-
-
-    def reset():
-        self.__init__()
-
+        self.n = np.zeros(k_arms)  # number of times each action was taken
+        self.q = np.full(k_arms, initial_q, dtype=float)  # values estimates for each action
 
     def update(self, a, r):
         self.n[a] += 1
         step_size = 1.0 / self.n[a]
-        self.q[a] = self.q[a] + step_size*(r - self.q[a])
+        self.q[a] = self.q[a] + step_size * (r - self.q[a])
 
 
 class ConstantStepSizeEstimator:
@@ -38,17 +34,10 @@ class ConstantStepSizeEstimator:
     Keeps and updates estimates Q(a) for all arms a of a k-armed bandit.
     """
 
-    def __init__(self, k_arms, initial_q=0, step_size=0.1):
+    def __init__(self, k_arms, *, initial_q=0, step_size=0.1):
         self.k = k_arms
-        self.n = np.zeros(k_arms)           # number of times each action was taken
-        self.q = np.full(k_arms, initial_q) # values estimates for each action
-        self.step_size = step_size          # step size for the update after n steps
-
-
-    def reset():
-        self.__init__()
-
+        self.q = np.full(k_arms, initial_q, dtype=float)  # values estimates for each action
+        self.step_size = step_size  # step size for the update after n steps
 
     def update(self, a, r):
-        self.n[a] += 1
-        self.q[a] = self.q[a] + self.step_size*(r - self.q[a])
+        self.q[a] = self.q[a] + self.step_size * (r - self.q[a])
