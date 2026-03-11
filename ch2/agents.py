76050ec758388ee318c62a0497485d8a915bac59
alex-ortan <13038064+alex-ortan@users.noreply.github.com.>
2026-03-11 12:32:44 -0400

    add possibility of setting seed explicitly for both bandit and agent

--- ch2/agents.py
+++ ch2/agents.py
@@ -8,7 +8,7 @@ class EpsilonGreedyAgent:
     Implements the epsilon-greedy algorithm for k-armed bandits.
     """
 
-    def __init__(self, k_arms=10, epsilon=0.1, estimator: Estimator = None):
+    def __init__(self, k_arms=10, *, epsilon=0.1, estimator: Estimator = None, seed=42):
         """
         Initializes the agent.
 
@@ -16,11 +16,13 @@ class EpsilonGreedyAgent:
             k_arms: number of arms in the bandit
             epsilon: Probability of exploration (0 <= epsilon <= 1).
             estimator: Initialized estimator class used to estimate values (e.g., SampleAverageEstimator).
+            seed: seed for random number generator
         """
 
         self.k = k_arms
         self.epsilon = epsilon
         self.estimator = estimator
+        self.rng = np.random.default_rng(seed)
 
     def select_action(self):
         """
@@ -28,13 +30,13 @@ class EpsilonGreedyAgent:
         It uses the Q values stored in the estimator.
         """
 
-        if np.random.rand() < self.epsilon:
+        if self.rng.random() < self.epsilon:
             # Explore: Choose a random action
-            action = np.random.randint(self.k)
+            action = self.rng.integers(self.k)
         else:
             # Exploit: Choose a greedy action based on current q values, breaking ties randomly
             actions = np.flatnonzero(self.estimator.q == self.estimator.q.max())
-            action = np.random.choice(actions).item()
+            action = self.rng.choice(actions).item()
 
         return action
 
--- ch2/bandit.py
+++ ch2/bandit.py
@@ -1,16 +1,12 @@
 import numpy as np
 
-# Random number generator initialized with fixed seed
-# This is at the top so every time a Bandit is initialized it can generate different rewards
-Rng = np.random.default_rng(seed=42)
-
 
 class Bandit:
     """
     Defines the bandit environment
     """
 
-    def __init__(self, k, q_true_mean=0, q_true_var=1, q_var=1, drift_var=0.0):
+    def __init__(self, k, *, q_true_mean=0, q_true_var=1, q_var=1, drift_var=0.0, seed=42):
         """
         k-armed bandit, where the rewards from each arm are normally distributed, with random means and variance 1.
         The mean rewards for each arm themselves are drawn from a normal distribution with mean 0 and variance 1.
@@ -21,13 +17,15 @@ class Bandit:
             q_true_var: variance of the q_true values each of the k arms
             q_var: variance of the sampled rewards; their mean is q_true
             drift_var: if the bandit is non stationary, the q_true values take a random walk with variance drift_var and mean 0 at every step
-
+            seed: seed for random number generator
         """
 
         self.k = k
         self._drift_var = drift_var
+        self.rng = np.random.default_rng(seed)
 
-        self._q_true = Rng.normal(q_true_mean, q_true_var, k)
+
+        self._q_true = self.rng.normal(q_true_mean, q_true_var, k)
         self._q_var = [q_var] * k
         self._best_action = np.argmax(self._q_true)
 
@@ -37,7 +35,7 @@ class Bandit:
         This is useful in non-stationary scenarios where calling self.reward() also changes the distribution.
         """
 
-        return Rng.normal(self._q_true[action], self._q_var[action], n)
+        return self.rng.normal(self._q_true[action], self._q_var[action], n)
 
     def reward(self, action):
         """
@@ -51,6 +49,6 @@ class Bandit:
         r = self.sample(action).item()
 
         # Add non-stationarity through a random walk of the true q values
-        self._q_true += Rng.normal(0, self._drift_var, self.k)
+        self._q_true += self.rng.normal(0, self._drift_var, self.k)
 
         return r
